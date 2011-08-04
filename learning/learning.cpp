07bbcd27c412c59fbbc0ff0002ebde10fd9e26f3
thomas <thomas@r2d2.(none)>
2011-08-04 14:18:33 +0200

    learning classes changed

--- learning/learning.cpp
+++ learning/learning.cpp
@@ -4,15 +4,3 @@
  *  Created on: 04.08.2011
  *      Author: thomas
  */
-
-#include "learning.h"
-
-learning::learning() {
-	// TODO Auto-generated constructor stub
-
-}
-
-learning::~learning() {
-	// TODO Auto-generated destructor stub
-}
-
