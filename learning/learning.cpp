e5673305e8e2a96014ccdd75506e12a26ba1e1ee
Thomas <mayrt@web.de>
2011-08-04 12:36:30 +0200

    learning folder added

---
+++ learning/learning.cpp
@@ -0,0 +1,18 @@
+/*
+ * learning.cpp
+ *
+ *  Created on: 04.08.2011
+ *      Author: thomas
+ */
+
+#include "learning.h"
+
+learning::learning() {
+	// TODO Auto-generated constructor stub
+
+}
+
+learning::~learning() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ learning/learning.h
@@ -0,0 +1,17 @@
+/*
+ * learning.h
+ *
+ *  Created on: 04.08.2011
+ *      Author: thomas
+ */
+
+#ifndef LEARNING_H_
+#define LEARNING_H_
+
+class learning {
+public:
+	learning();
+	virtual ~learning();
+};
+
+#endif /* LEARNING_H_ */
